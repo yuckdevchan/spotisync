fef8ea66ccc7fa57d35d8667817306de31d3f8e0
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2025-09-16 17:18:01 +0100

    a

---
+++ main.py
@@ -0,0 +1,27 @@
+from urllib.parse import quote
+import requests
+
+def match_song(url: str, service: str = "spotify", region: str = "GB"):
+    response = requests.get(
+        f"https://api.song.link/v1-alpha.1/links?url={quote(url)}&userCountry={region}&songIfSingle=true"
+    )
+    entities_by_unique_id = response.json()["entitiesByUniqueId"]
+    for service_key in entities_by_unique_id.keys():
+        if service_key.startswith(f"{service.upper()}_SONG::"):
+            return entities_by_unique_id[service_key]["id"]
+    return None
+
+def spotify_play_song(id: str):
+    requests.get(f"http://localhost:8801/playTrack/spotify:track:{id}")
+
+def spotify_pause():
+    requests.get(f"http://localhost:8801/pause")
+
+def spotify_play():
+    requests.get(f"http://localhost:8801/play")
+
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=kVgKio0z1hY"
+    id = match_song(url)
+    if id != None:
+        play_song_on_spotify(id)
